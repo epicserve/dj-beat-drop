e2b4b49f983de541a12b25bb9c3b79e45b7d1609
Brent O'Connor <epicserve@gmail.com>
2024-10-20 14:25:41 -0500

    Switch to using Typer instead of Argparse

--- src/dj_beat_drop/main_cli.py
+++ src/dj_beat_drop/main_cli.py
@@ -1,7 +1,8 @@
-import argparse
+import importlib.metadata
 
 import requests
-from pkg_resources import get_distribution, parse_version
+import typer
+from packaging.version import parse
 
 from dj_beat_drop.new import handle_new
 from dj_beat_drop.utils import color
@@ -9,23 +10,35 @@ from dj_beat_drop.utils import color
 
 def get_ascii_logo():
     logo = r"""
-   ___     __  ___           __    ___              
-  / _ \__ / / / _ )___ ___ _/ /_  / _ \_______  ___ 
+   ___     __  ___           __    ___
+  / _ \__ / / / _ )___ ___ _/ /_  / _ \_______  ___
  / // / // / / _  / -_) _ `/ __/ / // / __/ _ \/ _ \
 /____/\___/ /____/\__/\_,_/\__/ /____/_/  \___/ .__/
-                                             /_/
-"""
+                                             /_/     """
     return logo
+
+
+def get_current_version():
+    return importlib.metadata.version("dj-beat-drop")
+
+
+main_command = typer.Typer(no_args_is_help=True)
+
+
+def print_version(value: bool):
+    if value:
+        print(f"Version {get_current_version()}")
+        raise typer.Exit()
 
 
 def check_version():
     package_name = "dj-beat-drop"
-    current_version = get_distribution(package_name).version
+    current_version = get_current_version()
 
     response = requests.get(f"https://pypi.org/pypi/{package_name}/json", timeout=10)
     latest_version = response.json()["info"]["version"]
 
-    if parse_version(current_version) < parse_version(latest_version):
+    if parse(current_version) < parse(latest_version):
         color.green(
             f"\033[0;33m\nA new version of {package_name} is available ({latest_version}). You are using "
             f"{current_version}. To update, run:\n\033[0m"
@@ -33,43 +46,33 @@ def check_version():
         print(f"  pip install --upgrade {package_name}\n")
 
 
-def main():
-    print(f"\033[38;2;255;165;0m{get_ascii_logo()}\033[0m")
-    check_version()
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "-v",
+@main_command.callback()
+def main_callback(
+    version: bool = typer.Option(
+        None,
         "--version",
-        action="version",
-        version=f"Version {get_distribution('dj-beat-drop').version}",
-    )
-    subparsers = parser.add_subparsers(dest="command")
+        "-v",
+        callback=print_version,
+        is_eager=True,
+        help="Show the application's version and exit.",
+    ),
+):
+    pass
 
-    # Add 'new' subcommand
-    new_parser = subparsers.add_parser("new", help="Create a new Django project.")
-    new_parser.add_argument(
-        "name",
-        type=str,
-        nargs="?",
-        help="Project name (e.g. 'example_project' or 'example-project').",
-    )
-    new_parser.add_argument(
-        "--lts",
-        action="store_true",
-        help="Use the latest LTS version of Django.",
-    )
-    new_parser.add_argument(
-        "--overwrite",
-        action="store_true",
-        help="Overwrite the project directory if it already exists.",
-    )
 
-    args = parser.parse_args()
+@main_command.command()
+def new(
+    name: str | None = typer.Argument(None, help="Project name (e.g. 'example_project' or 'example-project')."),
+    use_lts: bool = typer.Option(False, "--lts", help="Use the latest LTS version of Django."),
+    overwrite: bool = typer.Option(False, "--overwrite", help="Overwrite the project directory if it already exists."),
+):
+    handle_new(name, use_lts, overwrite)
 
-    if args.command == "new":
-        handle_new(args.name, args.lts, args.overwrite)
-    else:
-        parser.print_help()
+
+def main():
+    color.orange(get_ascii_logo())
+    check_version()
+    main_command()
 
 
 if __name__ == "__main__":
